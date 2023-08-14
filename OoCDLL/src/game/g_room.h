ac47b6645a7a42e9d2339eb026ac5f2e3e8c72d6
TheBlizWiz <david.m.rossington@gmail.com>
2023-08-13 19:24:39 -0700

    idk why but pulling isnt working so i manually copied to my desktop

--- OoCDLL/src/game/g_room.h
+++ OoCDLL/src/game/g_room.h
@@ -13,4 +13,11 @@ struct Room {
     Tile_t *tiles[ROOM_SIZE_X][ROOM_SIZE_Y];
 };
 
+
+Room_t *room_new(u64 tags, u8 id);
+Error_t room_free(Room_t *room, App_t *app);
+Error_t room_draw(Room_t *room, App_t *app);
+Error_t room_drawfloor(Room_t *room, App_t *app);
+Error_t room_drawtiles(Room_t *room, App_t *app);
+
 #endif
